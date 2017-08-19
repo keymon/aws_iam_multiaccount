f53f3b80a8c802957286c3ac5d21bd937c86d902
Hector Rivas Gandara <keymon@gmail.com>
2017-08-19 18:57:48 +0100

    Configure backend config for this repo
    From running ./run_terraform.sh init-backend

---
+++ backend_config.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+	bucket         = "terraform-tfstate-246505230303"
+	key            = "aws_iam_multiaccount"
+	region         = "eu-west-1"
+	dynamodb_table = "terraform_locks"
+  }
+}
