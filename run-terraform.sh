e001f2841727ad4a864d1f042b040a0d0f02e12a
Hector Rivas Gandara <keymon@gmail.com>
2017-09-29 17:32:51 +0100

    Add the sandbox subaccount id from pass

--- run-terraform.sh
+++ run-terraform.sh
@@ -7,7 +7,9 @@ cd "$(dirname "$0")"
 
 export TF_VAR_root_account_id="$(pass keytwine/aws/root/account_id)"
 export TF_VAR_allowed_ips="$(pass keytwine/aws/allowed_ips.json)"
-export TF_VAR_sub_account_ids="[]"
+export TF_VAR_sub_account_ids="[
+	\"$(pass keytwine/aws/sandbox/account_id)\"
+]"
 
 case "${1:-}" in
   init-backend)
